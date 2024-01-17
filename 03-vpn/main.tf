c14bf4b4e6c44bbd4d1e5662c0b6da397067860b
dev1 <dev1@email.com>
2024-01-17 13:59:09 +0530

    terraform-module-vpn

---
+++ 03-vpn/main.tf
@@ -0,0 +1,18 @@
+module "vpn" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  name                   = "${local.ec2_name}-vpn"
+  ami = data.aws_ami.centos8.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+  subnet_id              = data.aws_subnet.selected.id
+  user_data = file("openvpn.sh")
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "vpn"
+    },
+    {
+      Name = "${local.ec2_name}-vpn"
+    }
+  )
+}
